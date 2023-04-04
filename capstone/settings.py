70a98da87ab7ab4d652c86372995d0d261fcdd64
kudzait <kudzai.tsapo@boxfusion.io>
2023-04-04 20:38:48 +0200

    Added crispy forms

--- capstone/settings.py
+++ capstone/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
@@ -122,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -132,3 +139,8 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  # new
+CRISPY_TEMPLATE_PACK = "bootstrap5"  # new
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 # pages/urls.py
 from django.urls import path
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
 ]
