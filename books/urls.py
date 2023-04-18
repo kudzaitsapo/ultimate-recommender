a424d7d352e5a14e22ffb697e031810c8be4b9a8
kudzait <kudzai.tsapo@boxfusion.io>
2023-04-18 15:57:53 +0200

    Added images to index page

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from .views import BookDetailView, BookListView, LikedBooksView, RecommendedBooksView, index, like_book
 
 urlpatterns = [
-    path("", index, name="index_page"),
+    path("", index, name="book_index_page"),
     path("all-books/", BookListView.as_view(), name="book_list"),
     path("liked-books/", LikedBooksView.as_view(), name="liked_books"),
     path("view/<uuid:pk>", BookDetailView.as_view(), name="book_detail"),
